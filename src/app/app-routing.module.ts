f61115cb9936be90063f08a6b8881a109c1517bc
Gauri <igaurigaidhane2002@gmail.com>
2023-09-20 22:32:57 +0530

    project implementations

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './component/home-page/home-page.component';
 import { PageNotFoundComponent } from './component/page-not-found/page-not-found.component';
+import { MedicineHomeComponent } from './component/medicine-home/medicine-home.component';
+import { ViewProductDetailsComponent } from './component/view-product-details/view-product-details.component';
+import { ViewProductDetailsCategoryComponent } from './component/view-product-details-category/view-product-details-category.component';
 
 const routes: Routes = [
-  {path:"",redirectTo:'/homepage',pathMatch:'full'},
   {path:"homepage",component:HomePageComponent},
-  {path:"**",component:PageNotFoundComponent}
+  {path:"medicine",component:MedicineHomeComponent},
+  {path:'view-product-details/:drug-code',component:ViewProductDetailsComponent},
+  {path:'view-top-details-by-category',component:ViewProductDetailsCategoryComponent},
+  {path:"",redirectTo:'/homepage',pathMatch:'full'},
+  {path:"**",component:PageNotFoundComponent},
+  
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,16 @@ import { HomePageComponent } from './component/home-page/home-page.component';
 import { HeaderComponent } from './component/header/header.component';
 import { FooterComponent } from './component/footer/footer.component';
 import { PageNotFoundComponent } from './component/page-not-found/page-not-found.component';
+import{BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { TopDealsComponent } from './component/top-deals/top-deals.component'
+import { CarouselModule } from 'ngx-owl-carousel-o';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { MedicineHomeComponent } from './component/medicine-home/medicine-home.component';
+import { ViewProductDetailsCategoryComponent } from './component/view-product-details-category/view-product-details-category.component';
+import { ViewProductDetailsComponent } from './component/view-product-details/view-product-details.component';
+import { LoginModule } from './login/login.module';
+
 
 @NgModule({
   declarations: [
@@ -14,11 +24,25 @@ import { PageNotFoundComponent } from './component/page-not-found/page-not-found
     HomePageComponent,
     HeaderComponent,
     FooterComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    MedicineHomeComponent,
+    TopDealsComponent,
+    MedicineHomeComponent,
+    ViewProductDetailsComponent,
+    ViewProductDetailsCategoryComponent,
+    
   ],
+  
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    CarouselModule,
+    FormsModule,
+    HttpClientModule,
+    LoginModule,
+   
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
